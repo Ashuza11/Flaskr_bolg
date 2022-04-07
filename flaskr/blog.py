dcf4dee57d569bbfa46eeed948bdc362590330f7
M ash <ashuza1411@gmail.com>
2022-04-07 08:12:55 +0200

    Add blog view and it static files

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -45,7 +45,7 @@ def create():
 
     return render_template('blog/create.html')
 
-## Get the post form the db
+## Get the post form the db once for using it to update and delete post
 def get_post(id, check_author=True):
     post = get_db().execute(
         'SELECT p.id, title, body, created, author_id, username'
@@ -61,3 +61,42 @@ def get_post(id, check_author=True):
         abort(403)
 
     return post
+
+## Upadate post 
+@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+@login_required
+def update(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE post SET title = ?, body = ?'
+                ' WHERE id = ?',
+                (title, body, id)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template('blog/update.html', post=post)
+
+
+## Delete 
+@bp.route('/<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id):
+    get_post(id)
+    db = get_db()
+    db.execute('DELETE FROM post WHERE id = ?', (id,))
+    db.commit()
+    return redirect(url_for('blog.index'))
